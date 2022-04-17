45e871806a6e4997c44302eeeb4810f0fe5f7580
tanafc <alu0101320878@ull.edu.es>
2022-04-17 02:34:35 +0100

    Añadiendo Firebase Backend

---
+++ introduccion_materialize/firebase.js
@@ -0,0 +1,36 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+  apiKey: "AIzaSyBm1MbDMCo-sx0LFX9LpadcRkyAtcscKR4",
+  authDomain: "proyecto-web-uya-4b07e.firebaseapp.com",
+  projectId: "proyecto-web-uya-4b07e",
+  storageBucket: "proyecto-web-uya-4b07e.appspot.com",
+  messagingSenderId: "458414668996",
+  appId: "1:458414668996:web:d70f4810512592d6171d91"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+
+export const saveUserData = (name, surname, email, year, location) => {
+  console.log(name, surname, email, year, location);
+  
+  addDoc(collection(db, 'users'), {
+    name: name,
+    surname: surname,
+    email: email,
+    date_birth: year,
+    place_birth: location
+  });
+}
---
+++ introduccion_materialize/register.js
@@ -0,0 +1,13 @@
+import {saveUserData} from './firebase.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Se espera el submit del formulario para actualizar la database
+  $("#registro").submit(function() {
+    var name = $('#nombres').val();
+    var surname = $('#apellidos').val();
+    var year = $('#fecha_nacimiento').val();
+    var location = $('#lugar_nacimiento').val();
+    var email = $('#correo').val();
+    saveUserData(name, surname, year, location, email);
+  });
+});
