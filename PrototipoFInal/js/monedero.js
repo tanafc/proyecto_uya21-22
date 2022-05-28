75ec310ab813ea4ffd9ddf32b0f750fb3198f5a7
Vlatko Marchan <alu0101321141@ull.edu.es>
2022-05-28 19:37:28 +0100

    Añadimos un formulario preliminar

--- PrototipoFInal/js/monedero.js
+++ PrototipoFInal/js/monedero.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    $('ul.ventanas li a:first').addClass('active');
+    $('ul.ventanas li a:first').addClass('activar');
     $('.secciones article').hide();
     $('.secciones article:first').show();
 
     $('ul.ventanas li a').click(function () {
-        $('ul.ventanas li a').removeClass('active');
-        $(this).addClass('active');
+        $('ul.ventanas li a').removeClass('activar');
+        $(this).addClass('activar');
         $('.secciones article').hide()
 
         var activeTab = $(this).attr("href");
