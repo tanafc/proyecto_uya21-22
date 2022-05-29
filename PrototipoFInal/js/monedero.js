04f5ca952d491ae07e8275ff55c7ff93632f8f63
Vlatko Marchan <alu0101321141@ull.edu.es>
2022-05-29 16:00:34 +0100

    Nueva funcionalidad tabla

--- PrototipoFInal/js/monedero.js
+++ PrototipoFInal/js/monedero.js
@@ -1,3 +1,14 @@
+function clasifyPrice(coin) {
+    if(coin == "Cardano" || coin == "cardano" || coin == "ADA") {
+        return 1.2 
+    } else if (coin == "Bitcoin" || coin == "Bitcoin" || coin == "BTC") {
+        return 27368.36
+    } else {
+        return "No disponible"
+    }
+}
+
+
 $(document).ready(function () {
     $('ul.ventanas li a:first').addClass('activar');
     $('.secciones article').hide();
@@ -12,5 +23,42 @@ $(document).ready(function () {
         $(activeTab).show();
         return false
     });
+
+    
+    // Obtenemos los datos pasados al programa y los guardamos en un objeto.
+    $("#Añadir-Moneda").submit(function () {
+        let flag = false;
+        // Obtenemos los datos
+        let correo = $('#correo').val();
+        let nombre = $('#nombre').val();
+        let antiguoExchange = $('#antiguo-exchange').val();
+        let nombreMoneda = $('#Nombre-moneda').val();
+        let cantidad = $('#Cantidad').val();
+        if (cantidad != '' && correo != '' && nombre != '' &&  antiguoExchange != '' &&  nombreMoneda != '') {
+            flag = true;
+        }
+        const objeto = {
+            correo: `${correo}`,
+            nombre: `${nombre}`,
+            antiguoExchange: `${antiguoExchange}`,
+            nombreMoneda: `${nombreMoneda}`,
+            cantidad: `${cantidad}`,
+        }
+        console.log("B")
+        if (flag) {
+            console.log("C")
+            $("#confirmed-transaction").append("La transacción se ha completado con éxito.");
+            const divisas = $("#divisas-tabla")[0];
+            const newRowAmount = divisas.insertRow();
+            newRowAmount.innerHTML = `
+                <td>${objeto.nombreMoneda}</td>
+                <td>${objeto.cantidad}</td>
+                <td>${clasifyPrice(objeto.nombreMoneda)}</td>
+            `;
+        } else {
+            $("#confirmed-transaction").append("Error.");
+        }
+    });
 });
+
 
