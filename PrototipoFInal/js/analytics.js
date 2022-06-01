7a1f97305246ded0b11e2d86ec674310cc280c7e
Ian V.L <alu0101333946@ull.edu.es>
2022-06-01 16:05:29 +0100

    Update analytics.js

--- PrototipoFInal/js/analytics.js
+++ PrototipoFInal/js/analytics.js
@@ -417,7 +417,7 @@ document.addEventListener('DOMContentLoaded', function () {
     tooltip: {
       headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
       pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y:.1f} USD</b></td></tr>',
+        '<td style=" color:{series.color};padding:0"><b>{point.y:.1f} USD</b></td></tr>',
       footerFormat: '</table>',
       shared: true,
       useHTML: true
